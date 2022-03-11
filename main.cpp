dbea591d17259b8e86684ebeff7e0e6bdf927d99
Matvey24 <perelman.matvey@gmail.com>
2022-03-11 21:58:05 +0300

    added first sdl window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <SDL.h>
+int main(int argc, char** argv) {
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER)){
+        std::cout << "Error initialising\n";
+        return 1;
+    }
+    SDL_Window* window =
+            SDL_CreateWindow("Doska",
+                             SDL_WINDOWPOS_UNDEFINED,
+                             SDL_WINDOWPOS_UNDEFINED,
+                             1280, 720,
+                             SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
+    if(window == NULL){
+        std::cout << "Error making window\n";
+        return 1;
+    }
 
-int main() {
-    std::cout << "Hello, Worldjkhkjhkjh!" << std::endl;
+    SDL_Surface* surf = SDL_GetWindowSurface(window);
+    SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0, 255, 0));
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event event;
+    bool running = true;
+    while(running){
+        SDL_WaitEvent(&event);
+        switch(event.type){
+            case SDL_QUIT:
+                running = false;
+                break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
